f590e2d6399cf4ac351fe7ff163837df4ace74fa
Sam <sam@Kirks-iMac.local>
2017-06-01 11:48:52 -0400

    update wp config

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'limeadco_obia');
+define('DB_NAME', 'wp_obia');
 
 /** MySQL database username */
-define('DB_USER', 'limeadco_obia');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'Donutman2017');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
-define('DB_HOST', 'limeadvertisinginc.com');
+define('DB_HOST', '192.168.2.54');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
