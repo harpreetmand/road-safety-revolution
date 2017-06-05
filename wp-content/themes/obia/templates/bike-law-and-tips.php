c1f81cdaba7c1d17ac99acd658c72f3eba8644f4
Harpreet Mand <harpreet@limeadvertising.com>
2017-06-05 13:46:08 -0400

    finished bike law page with background image fixed

--- wp-content/themes/obia/templates/bike-law-and-tips.php
+++ wp-content/themes/obia/templates/bike-law-and-tips.php
@@ -1,6 +1,6 @@
 <?php /* Template Name: Bike law and tips Template */ get_header(); ?>
 <main role="main">
-  <section class="welcome-bikeLaw container" id="">
+  <section class="welcome-bikeLaw container" id="bikeLaw">
     <div class="top marb20">
         <div class="flex-item">
             <div class="bgImage">
@@ -20,7 +20,7 @@
     <div class="cf"></div>
 
 
-	<div id="myCarousel" class="carousel  slide">
+	<div id="myCarousel" class="carousel slide marb20">
   <!-- Dot Indicators -->
   <ol class="carousel-indicators">
     <li data-target="#myCarousel" data-slide-to="0" ></li>
@@ -40,8 +40,8 @@
     <div class="item"><span class="font"> 3. Collect Police Info</span><br/> <span class="small">Get the business card of the officer in charge.</span></div>
     <div class="item"><span class="font"> 4. Collect Driver Info</span><br/> <span class="small">Get the driver’s name and insurance information.</span></div>
     <div class="item"><span class="font"> 5. Accept Help</span><br/> <span class="small">Seek medical attention immediately. If offered an ambulance, accept!
-Police do not offer an ambulance unless they think you need it. If you think you may be injured (even if you are not sure whether it is serious)
-go to the ER!</span></div>
+        Police do not offer an ambulance unless they think you need it. If you think you may be injured (even if you are not sure whether it is serious)
+        go to the ER!</span></div>
     <div class="item"><span class="font"> 6. Record & Store Info</span><br/> <span class="small">Document your injuries: take photos and keep a diary.</span></div>
     <div class="item"><span class="font"> 7. Seek Legal Advice</span><br/> <span class="small">Never negotiate with an insurance company on your own. Get legal advice first.</span></div>
     <div class="item"><span class="font"> 8. Communicate Through A Lawyer</span><br/> <span class="small">Do not give a written or recorded statement without speaking to a lawyer first.</span></div>
@@ -56,7 +56,33 @@ go to the ER!</span></div>
         </a>
 </div>
 
+<div class="bikeLawFaq">
+    <div class="flex-item hidden-sm hidden-xs">
+        <div class="bgImage" id="pImage"></div>
+        <!--<img src="</?php echo get_template_directory_uri(); ?>/img/bike-law-faq.jpg" class="img-responsive"/>-->
+    </div>
+    <div class="flex-item">
+        <div class="faqBlack condensedFont marb20 ">
+            <div class="font100">
+                BIKE LAW FAQ
+            </div>
+        </div>
+        
+        <div class="yellowBack condensedFont"><strong>I don’t have insurance, can I still make a claim?</strong></div>
+        <p>Yes, in many cases, you can claim against the insurance company of the vehicle that struck you. You may also access the SABS (Statutory Accident Benefits Schedule) of the motor vehicle that struck you.</p>
+        <div class="yellowBack condensedFont"><strong>It was a hit-and-run, can I still start a lawsuit?</strong></div>
+        <p>Yes, you may be able to. The insurance regime in Ontario is set up to compensate victims of hit and runs, even if the driver remains unidentified. You may also be able to access SABS (Statutory Accident Benefits Schedule). </p>
+        <div class="yellowBack condensedFont"><strong>I was “doored”, is that a traffic accident?</strong></div>
+        <p>Yes. In Ontario, any incident arising from the normal use and operation of an automobile is covered by insurance. Depending on the circumstances, this may entitle you to a SABS (Statutory Accident Benefits Schedule) claim.</p>
+        <div class="yellowBack condensedFont"><strong>I wasn’t wearing a helmet, can I still make a claim?</strong></div>
+        <p>Yes. The law recognizes that nobody is perfect (including cyclists). Even if you have made a mistake, such as not wearing a helmet, or cycling through a stop sign, you may still be entitled to compensation.</p>
+        <div class="yellowBack condensedFont"><strong>A car caused the accident, but I did not hit the car, I hit another obstacle, can I start a lawsuit?</strong></div>
+        <p>Yes. If a driver causes an accident, even if the actual collision is with an object other than their car, they may be held liable.</p>
 
+        
+    </div>
+</div>
+<div class="cf"></div>
 
   </section>
 </main>
